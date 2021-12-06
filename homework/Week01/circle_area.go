b2eac6ceb72473484fd921933802867003ac4f5f
mwclg <zhoudl@mwclg.com>
2021-12-06 09:59:23 +0800

    极客作业提交: 第一周

---
+++ homework/Week01/circle_area.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func main() {
+	fmt.Println(time.Now())
+	fmt.Println("计算圆的面积....")
+
+	var r float64
+	fmt.Printf("请输入园半径: ")
+
+	for {
+		_, err := fmt.Scanln(&r)
+		if err != nil {
+			fmt.Println("输入必须是数字类型, 请重新输入! ")
+			continue
+		}
+
+		if r <= 0 {
+			fmt.Println("圆半径必须大于等于1, 请重新输入! ")
+			continue
+		} else {
+			fmt.Println("圆面积是: ", math.Pi*r*r)
+			break
+		}
+
+	}
+}
